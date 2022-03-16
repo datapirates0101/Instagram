77e2704193cf72959a0002d4ba036df065a9e5a4
datapirates0101 <datapirates0101@gmail.com>
2022-03-16 19:58:32 +0530

    search user function

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,71 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram/utils/colors.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController searchController = TextEditingController();
+  bool isShowUser = false;
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(
+          controller: searchController,
+          decoration: const InputDecoration(
+            labelText: 'Search for a user',
+          ),
+          onFieldSubmitted: (String _) {
+            setState(() {
+              isShowUser = true;
+            });
+          },
+        ),
+      ),
+      body: isShowUser
+          ? FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .where('name', isGreaterThanOrEqualTo: searchController.text)
+                  .get(),
+              builder: (context,
+                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          snapshot.data!.docs[index]['photoUrl'],
+                        ),
+                      ),
+                      title: Text(
+                        snapshot.data!.docs[index]['name'],
+                      ),
+                    );
+                  },
+                );
+              },
+            )
+          : const Text('Posts'),
+    );
+  }
+}
